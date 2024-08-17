963081788a54f924ccfb1e883e201d610d27a60e
MindfulLearner <dimajosh1009@gmail.com>
2024-08-17 03:04:35 +0200

    creazione lobby e page lobby con css

---
+++ src/app/frontpage/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./page.module.css";
+
+const lobby = () => {
+  return (
+    <main className={styles.swagger}>
+      <header>
+        <div className={styles.colors}>swagger</div>
+      </header>
+    </main>
+  );
+};
+
+export default lobby;
