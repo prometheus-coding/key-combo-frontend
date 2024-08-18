4fde306170521d1b1d526f70fe7a24f4520e093c
Ethan <ethanbone435@gmail.com>
2024-08-18 20:23:17 +0200

    fix styling e background image

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import styles from "./page.module.css";
-import "./globale.css";
-
+import styles from '../page.module.css'
 const Header = () => {
   return (
     <nav className={styles.navbar}>
