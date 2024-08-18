23ee40569b26934710bdbece7fd7db1f2aedd835
MindfulLearner <dimajosh1009@gmail.com>
2024-08-18 22:00:22 +0200

    aggiunti style componenets sistemato un po csseaggiungo debug globalecss

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,28 +1,78 @@
+"use client"; // serve questa cosa per farfuznioanre react su lato client che sul serer!!
+
 import React from "react";
-import styles from '../page.module.css'
+import styled from "styled-components";
+// debugger
+import "./global.css";
+
+const Navbar = styled.nav`
+  color: white;
+`;
+const NavbarBackgroundImage = styled.div`
+  background-image: url("/images/wallpapersden.com_happiest-anime-girl-hd-ai-art_1952x1120.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  height: 100vh;
+`;
+
+const NavbarContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 80px;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.45);
+`;
+
+const NavbarLinks = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  width: calc(100% / 3);
+  padding-left: 40px;
+`;
+
+const NavbarLogoContainer = styled.div`
+  width: calc(100% / 3);
+  display: flex;
+  justify-content: center;
+`;
+
+const NavbarUserContainer = styled.div`
+  display: flex;
+  width: calc(100% / 3);
+  justify-content: end;
+  padding-right: 10px;
+  gap: 5px;
+`;
+
 const Header = () => {
   return (
-    <nav className={styles.navbar}>
-      <div className={styles["navbar-background-image"]}>
-        <div className={styles["navbar-container"]}>
-          <div className={styles["navbar-links"]}>
-            <div className={styles["nav-item"]}>Home</div>
-            <div className={styles["nav-item"]}>Leaderboard</div>
-            <div className={styles["nav-item"]}>Community</div>
-            <div className={styles["nav-item"]}>Info</div>
-          </div>
-          <div className={styles["navbar-logo-container"]}>
-            <img src="" alt="logo" className={styles["navbar-logo"]} />
-          </div>
-          <div className={styles["navbar-user-container"]}>
-            <div className={styles["navbar-user-name"]}>User</div>
-            <div className={styles["navbar-img-container"]}>
-              <img src="" alt="logo" className={styles["user-profile-img"]} />
+    <Navbar>
+      <NavbarBackgroundImage>
+        <NavbarContainer>
+          <NavbarLinks>
+            <div>Home</div>
+            <div>Leaderboard</div>
+            <div>Community</div>
+            <div>Info</div>
+          </NavbarLinks>
+          <NavbarLogoContainer>
+            <img src="" alt="logo" />
+          </NavbarLogoContainer>
+          <NavbarUserContainer>
+            <div>User</div>
+            <div>
+              <img
+                src=""
+                alt="profile"
+                style={{ borderRadius: "50%", width: "40px", height: "40px" }}
+              />
             </div>
-          </div>
-        </div>
-      </div>
-    </nav>
+          </NavbarUserContainer>
+        </NavbarContainer>
+      </NavbarBackgroundImage>
+    </Navbar>
   );
 };
 
